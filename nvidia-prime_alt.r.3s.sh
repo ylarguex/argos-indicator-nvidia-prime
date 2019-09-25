7e2f5ec53815cef6114bc478494056b3dafd8846
ylarguex <55814291+ylarguex@users.noreply.github.com>
2019-09-25 17:06:25 -0500

    nvidia-prime_alt.r.3s.sh
    Hi,
    I have taken the liberty of making some minor changes to the script by adding the possibility of changing graphics cards without having to restart, a logout is enough. i tested it in Ubuntu 19.04 and it works perfectly.
    
    greetings,

---
+++ nvidia-prime_alt.r.3s.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+run_as_root="pkroot -d" # gksu; pkroot; gnomesu -c;
+notify_switch="notify-send -h int:transient:2 \\\"dialog-information-symbolic\\\" \\\"NVidia Prime Indicator\\\" \\\"Switching graphics. Please logout to finalize process! \\\" ; "
+notify_switch_and_reboot="notify-send -h int:transient:2 -i \\\"dialog-information-symbolic\\\" \\\"NVidia Prime Indicator\\\" \\\"Switching graphics. System will reboot soon... \\\" ; "
+notify_reboot="notify-send -h int:transient:2 -i \\\"dialog-information-symbolic\\\"  \\\"NVidia Prime Indicator\\\" \\\"System will reboot! \\\" ; "
+
+activate_intel="\"\
+	$notify_switch \
+	$run_as_root \\\"prime-select intel\\\"; \
+	sleep 1; \
+	gnome-session-quit --force \
+	\""
+
+activate_nvidia="\"\
+	$notify_switch \
+	$run_as_root \\\"prime-select nvidia\\\"; \
+	sleep 1; \
+	gnome-session-quit --force \
+	\""
+
+activate_intel_and_reboot="\"\
+	$notify_switch_and_reboot \
+	$run_as_root \\\"prime-select intel\\\"; \
+	$notify_reboot \
+	sleep 1; \
+	systemctl --no-wall reboot \
+	\""
+
+activate_nvidia_and_reboot="\"\
+	$notify_switch_and_reboot \
+	$run_as_root \\\"prime-select nvidia\\\"; \
+	$notify_reboot \
+	sleep 1; \
+	systemctl --no-wall reboot \
+	\""
+
+nvidia_settings="\"nvidia-settings -p 'PRIME Profiles'\""
+nvidia_active_icon="nvidia-active-symbolic"
+nvidia_inactive_icon="nvidia-inactive-symbolic"
+reboot_icon="system-restart"
+
+
+QUERY=$(prime-select query)
+if [ "$QUERY" == 'nvidia' ]; then
+    nvidia_state_icon=$nvidia_active_icon
+    TEMP=$(nvidia-smi -q -d TEMPERATURE | grep 'GPU Current Temp' | awk '{print $5}')
+    panel_string="$TEMP\xe2\x84\x83 | "
+else
+    nvidia_state_icon=$nvidia_inactive_icon
+    panel_string=" | "
+fi
+
+echo -e "$panel_string""iconName=$nvidia_state_icon"
+echo "---"
+echo "NVIDIA PRIME Profiles| iconName=$nvidia_state_icon bash=$nvidia_settings terminal=false"
+echo "---"
+echo "Switch to INTEL :: LOGOUT | iconName='prime-indicator-intel' bash=$activate_intel terminal=false"
+echo "Switch to NVIDIA :: LOGOUT | iconName='prime-indicator-nvidia' bash=$activate_nvidia  terminal=false"
+echo "---"
+echo "Switch to INTEL :: REBOOT | iconName='prime-indicator-intel' bash=$activate_intel_and_reboot terminal=false"
+echo "Switch to NVIDIA :: REBOOT | iconName='prime-indicator-nvidia' bash=$activate_nvidia_and_reboot  terminal=false"
